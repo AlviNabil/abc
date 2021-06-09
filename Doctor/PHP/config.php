843d68c6f9caa3018df111a9995527c96ba1ce05
Ryuga <alvinabil07@gmail.com>
2021-06-10 02:40:28 +0600

    deleted

--- Doctor/PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "chat");
-    if(!$conn){
-        echo "Database Not Connected" . mysqli_connect_error(); 
-    }
-    else{
-        
-    }
-?>
