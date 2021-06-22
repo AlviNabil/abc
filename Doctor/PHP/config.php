5ff1df9ff8b812ac548d7319859691234a737047
Ryuga <alvinabil07@gmail.com>
2021-06-22 16:28:24 +0600

    arifeen new

--- Doctor/PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "app");
-    if(!$conn){
-        echo "Database Not Connected" . mysqli_connect_error(); 
-    }
-    else{
-        
-    }
-?>
--- Patient/PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "App");
-    if(!$conn){
-        echo "Database Not Connected" . mysqli_connect_error(); 
-    }
-    else{
-        
-    }
-?>
